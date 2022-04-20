735755e009813672879ea2ab9c342cfd20e0cadb
Alastair <alastair05@gmail.com>
2022-04-19 18:31:55 -0700

    Add more tests and refactor DayListItem depending on availible spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,28 @@
 import React from "react";
-import "components/DayListItem.scss"
+import "components/DayListItem.scss";
+import classNames from "classnames";
 
 export default function DayListItem(props) {
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--select': props.selected,
+    'day-list__item--full': props.spots === 0});
+
+    const formatSpots = (props) => {
+      if (props.spots === 0) {
+        return <h3 className="text--light">no spots remaining</h3>;
+      }
+      if (props.spots === 1) {
+        return <h3 className="text--light">{props.spots} spot remaining</h3>;
+      }
+      if (props.spots > 1) {
+        return <h3 className="text--light">{props.spots} spots remaining</h3>;
+      }
+    };
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      {formatSpots(props)}
     </li>
   );
 }
