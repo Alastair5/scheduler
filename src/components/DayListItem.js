48590a3455adbabce930f8ee0f194ce515f85e79
Alastair <alastair05@gmail.com>
2022-04-19 19:12:48 -0700

    Add DayList with dynamic day selection

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,14 +7,14 @@ export default function DayListItem(props) {
     'day-list__item--select': props.selected,
     'day-list__item--full': props.spots === 0});
 
-    const formatSpots = (props) => {
-      if (props.spots === 0) {
+    const remainingSpots = (spots) => {
+      if (spots === 0) {
         return <h3 className="text--light">no spots remaining</h3>;
       }
-      if (props.spots === 1) {
+      if (spots === 1) {
         return <h3 className="text--light">{props.spots} spot remaining</h3>;
       }
-      if (props.spots > 1) {
+      if (spots > 1) {
         return <h3 className="text--light">{props.spots} spots remaining</h3>;
       }
     };
@@ -22,7 +22,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      {formatSpots(props)}
+      {remainingSpots(props.spots)}
     </li>
   );
 }
